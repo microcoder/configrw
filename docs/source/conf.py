3ca72d8dded1f016a96c996bb3c5e50979035a91
Vladimir Demenev <vademenev@gmail.com>
2019-04-10 12:29:23 +0300

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes: http://www.sphinx-doc.org/en/stable/theming.html
 #
-html_theme = 'nature'
+html_theme = 'alabaster' # nature
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
